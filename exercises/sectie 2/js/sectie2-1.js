3c87ca1193d0633a7760df0beda411c2a2511f2f
Anna Masala <Annavalentina2006.am@gmail.com>
2023-02-13 17:11:36 +0100

    sectie 2-1 js

---
+++ exercises/sectie 2/js/sectie2-1.js
@@ -0,0 +1,106 @@
+//ik gebruik willekeurige getallen
+//1
+var roc = 7;
+var aeres = 6;
+var diploma = 8;
+
+console.log(roc);
+console.log(aeres);
+console.log(diploma);
+
+//2
+var age = 16;
+
+console.log(age);
+
+//3
+//de const variabele kan datatypen zoals nummers en letters opslaan. het kan alleen niet tijdens uitvoering worden veranderd. in dit geval word strinf gebruikt
+
+//4
+//er word een datatype gebruikt dat nummers kan opslaan
+
+//5
+//er word string gebruikt
+
+//6
+//er word een boolean variabele gebruikt
+
+//7
+//string, want het getal is binnen haakjes
+
+//8
+//weer string
+
+//9
+//
+
+//10
+console.log(typeof "konijn");
+
+//11
+let myAge = 2 * 8;
+
+//12
+console.log(myAge);
+
+//13
+console.log(myAge * 2);
+
+//14
+console.log(age % 8);
+
+//15
+const street = "Iskurstraat ";
+const number = 3;
+
+console.log(street + number);
+
+//16
+let numberOne = 12;
+console.log(numberOne + 3);
+
+numberOne + 3;
+console.log(numberOne);
+
+//17
+console.log(numberOne * 15);
+numberOne * 15;
+console.log(numberOne);
+
+//18
+numberOne + 1;
+console.log(numberOne);
+
+numberOne++;
+console.log(numberOne);
+
+console.log(numberOne + 1);
+
+//19
+numberOne - 1;
+console.log(numberOne);
+
+numberOne--;
+console.log(numberOne);
+
+console.log(numberOne - 1);
+
+//20
+let vader = 183 * 3;
+let moeder = 178;
+let kind = vader + moeder;
+console.log(kind / 4);
+
+//21
+let vader2 = 192;
+let moeder2 = 153;
+let kind2 = vader2 + moeder2;
+console.log(kind2 / 4);
+
+//22
+let length;
+let BMI = 28.6;
+let weight = 93;
+
+//23
+//ik weet niet hoe ik dit moet berekenen, maar geen zorgen ik weet wel hoe ik het zou moeten programmeren
