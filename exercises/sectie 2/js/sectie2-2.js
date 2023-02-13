65c3a7cecd3351f37ba10be50900d44eed99dbf3
Anna Masala <Annavalentina2006.am@gmail.com>
2023-02-13 17:39:02 +0100

    sectie 2-2 js

---
+++ exercises/sectie 2/js/sectie2-2.js
@@ -0,0 +1,63 @@
+//1
+let fn = "Anna ";
+let ln = "Masala";
+let age = 16;
+
+console.log(fn, ln, age);
+
+//2
+console.log("Mijn naam is " + fn);
+
+//3
+console.log("Mijn volledige naam is " + fn + ln);
+
+//4
+console.log("In 2056 ben ik " + (age + 36));
+
+//5
+console.log(
+  "Mijn naam is " +
+    fn +
+    "en ik ben " +
+    age +
+    " jaar oud. daarnaast is mijn familienaam " +
+    ln
+);
+
+//6
+let a = "de console";
+let b = "donderdag";
+let c = "chips eten op de bank.";
+
+console.log(
+  "Wat ik doe log ik naar " +
+    a +
+    "als het " +
+    b +
+    "is ga ik kickboxen, anders ga ik naar huis " +
+    c
+);
+
+//7
+//ik begrijp de vraagstelling niet helemaal dus ik doe het op verschillende manieren.
+
+let zin1 = "Wat ik doe log ik in de console. ";
+let zin2 = "Als het regent, dan neem ik mijn pareplu mee naar buiten. ";
+let zin3 = "Anders zet ik mijn zonnebril op.";
+
+console.log(zin1 + zin2 + zin3);
+
+//8
+//dit is waarschijnlijk heel time consuming maarja
+let ik = "ik ";
+
+console.log(
+  zin1 +
+    "Als het gras geel is, dan geef " +
+    ik +
+    "het water. Anders doe " +
+    ik +
+    "niks."
+);
+
+//9
