c59a7aa22a9ead2e23708f2e746576c478021112
Anna Masala <Annavalentina2006.am@gmail.com>
2023-02-15 11:24:43 +0100

    sectie 3-1 js

---
+++ exercises/sectie 2/js/sectie3-1.js
@@ -0,0 +1,120 @@
+//1
+/*stric mode in javascript optimaliseert de code en is meer
+streng als het gaat om kleine foutjes die iemand over het hoofd
+ziet.*/
+//2
+//het kan worden aangegeven aan het begin van een functie of script
+
+//3
+/*het kan kleine problemen oplossen die vaak niet echt uitmaken, maar
+wel problemen kunnen zorgen bij sommige javascript engines die bepaalde
+soort syntax niet accepteren. het laat je programma sneller runnen, maar
+het zorgt er wel voor dat bepaalde tussenwegen niet kunnen worden gebruikt.*/
+
+//4
+//een functie is een taak die kan worden uitgevoerd op bepaalde omstandigheden.
+//of om beter te zeggen, je kan een functie oproepen
+
+//5
+/*in een variabele kan je waardes opslaan, terwijl je bij een functie 
+die variabelen als parameter kan gebruiken*/
+
+//6
+//
+
+//7
+//
+
+//8
+//niks, je hebt de functie nog niet een doel gegeven
+
+//9
+//Cheese
+
+//10
+//cheese
+//cheese
+
+//11
+//Je krijgt 3 knuffels van mij
+
+//12
+//je krijgt 12 knuffels van mij
+
+//13
+/*je krijgt 1 knuffel van mij. want de > operator
+zorgt ervoor dat als het nummer boven de 1 is knuffels
+word weergegeven*/
+
+//14
+//je krijgt 12 knuffels van mij. nu dat het getal boven 1 is word knuffels weergegeven
+
+//15
+//je krijgt 12 extra knuffels van mij. de operator voegt toe dat als de naam jorn is er extra knufels word weergeven
+
+//16
+/*je krijgt 12 knuffels van mij. de naam jorn word daar niet met hoofdletter geschreven
+dus word de "extra knuffels" geschrapt*/
+
+//17
+//nog niks, je hebt het nog niet aangeroepen
+
+//18
+//result
+
+//19
+//4
+
+//20
+//8
+
+//21
+//20
+
+//22
+//wanneer je het resultaat oproept
+
+//23
+//niks, want hij returned het resultaat niet
+
+//24
+//een declaration kan je door je hee code gebruiken, en een expression kan je op bepaalde plekken gebruiken, om effiecient te zijn.
+
+//25
+//een parameter is een van de variabelen in een functie, terwijl argumenten de data zijn.
+
+//26
+//het is een functie maar dan beter en efficienter
+
+//27
+//je grebruikt vaak => operators bij arrow functions en die lijken om arrows
+
+//28
+//een nomale functie kan je zelf opmaken en een arrow functie niet.
+
+//29
+//5
+
+//30
+//8
+
+//31
+//34
+
+//32
+//henk works as a teacher and henk is happy
+
+//33
+//frist works as a developer and frits is very happy
+
+//34
+//henk works as a teacher and henk is very
+
+//35
+//om de herhaling in het programmeren tegen te gaan
+
+//36
+//het gaat een line naar beneden
+
+//37
+//de console.og zit in de fucntie zelf, terwijl als je het zou willen aanroepen het buiten de functie hoort te staan.
